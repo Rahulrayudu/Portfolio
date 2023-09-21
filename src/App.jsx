cd9a5cf39f7a24d16d28272bef1abc95d8c7db8e
Rahul Sandireddy <76141102+Rahulrayudu@users.noreply.github.com>
2023-09-21 17:09:51 +0530

    protfolio

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,8 @@ import { BrowserRouter } from "react-router-dom";
 
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
-
 const App = () => {
   return (
-    <>
     <BrowserRouter>
       <div className='relative z-0 bg-primary'>
         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
@@ -13,19 +11,14 @@ const App = () => {
           <Hero />
         </div>
         <About />
-        <Experience />
-        <Tech />
         <Works />
-        <Feedbacks />
         <div className='relative z-0'>
           <Contact />
           <StarsCanvas />
         </div>
       </div>
     </BrowserRouter>
-    </>
   );
 }
 
 export default App;
-
